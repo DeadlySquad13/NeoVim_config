06d6058ac3f8eacd2b23e8af67ebc02d8e974fc7
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2022-12-08 19:45:45 +0300

    [Enh](FireNvim): Added mappings and updated settings for focusing, made github fields as markdown by default.

--- lua/config/firenvim.lua
+++ lua/config/firenvim.lua
@@ -1,3 +1,4 @@
+-- Extend text input up to 10 lines if it's small.
 vim.cmd([[
   function! s:IsFirenvimActive(event) abort
     if !exists('*nvim_get_chan_info')
@@ -19,3 +20,35 @@ vim.cmd([[
     autocmd UIEnter * call OnUIEnter(deepcopy(v:event))
   augroup end
 ]])
+
+vim.g.firenvim_config = {
+  localSettings = {
+    -- Use [keyboard shortcut](https://github.com/glacambre/firenvim#manually-triggering-firenvim) inside browser to start editing input in firenvim.
+    ['.*'] = { takeover = 'never' },
+  },
+}
+
+-- Use different settings depending on the page being edited.
+local utils = require('utils')
+local create_augroup, create_autocmd = utils.create_augroup, utils.create_autocmd
+
+local Firenvim = create_augroup('Firenvim', { clear = true })
+
+create_autocmd({ 'BufEnter' }, {
+  group = Firenvim,
+  pattern = 'github.com_*.txt',
+  desc = 'Started editing GitHub text element',
+  callback = function() vim.bo.filetype = 'markdown' end
+})
+
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+-- Keymappings.
+map('n', '<esc><esc>', ':call firenvim#focus_page()<cr>')
+map('n', '<c-z>', ':call firenvim#hide_frame()<cr>')
