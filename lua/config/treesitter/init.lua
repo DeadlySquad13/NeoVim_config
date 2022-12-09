551fba97ed653cb4e3bd1574fbad6ec14dbcae3b
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2022-12-09 09:23:50 +0300

    [Fix](Queries): Removed custom queries initialization. Left comments for now.

--- lua/config/treesitter/init.lua
+++ lua/config/treesitter/init.lua
@@ -20,9 +20,6 @@ local incremental_selection = require_treesitter_configuration(
 )
 local rainbow = require_treesitter_configuration('rainbow')
 
-package.path = require('constants.env').NVIM_AFTER .. '/?.lua;' .. package.path
-require('queries.init')
-
 tree_sitter.setup({
   ensure_installed = parsers,
   ignore_install = {}, -- List of parsers to ignore installing
