2f5fd9af7ca38a106a623317fc6260b576e98766
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2022-12-10 23:39:29 +0300

    [Enh](Web): Added scss and css indent settings into ftplugin.

---
+++ after/ftplugin/css/init.lua
@@ -0,0 +1,6 @@
+local set_settings = require('utils.setters').set_settings
+
+set_settings({
+  shiftwidth = 4, -- Set number of spaces per auto indentation
+  tabstop = 4, -- Number of spaces that a <Tab> in the file counts for.
+})
--- lua/init.lua
+++ lua/init.lua
@@ -1,3 +1,12 @@
+--- Add folder to the list of possible paths which is used during module
+--requirement.
+-- It will be added as a simple module pattern (either `path.lua` or
+--`path/init.lua`).
+---@param path (string) # Folder to add.
+local function append_to_package_path(path)
+  package.path = path .. '/?.lua;' .. path .. '/?/init.lua;' .. package.path
+end
+append_to_package_path(require('constants.env').NVIM_AFTER)
 require('general_settings')
 require('config.commands')
 require('autocommands')
