6e8e2ed50f608ec32b2f9692153a8839ac3dc2b6
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-01-08 13:19:59 +0300

    [Fix](Ui): Changed require path.

--- lua/ds_omega/layers/Ui/which_key.lua
+++ lua/ds_omega/layers/Ui/which_key.lua
@@ -1,6 +1,6 @@
 local simple_plugin_setup = require('ds_omega.utils').simple_plugin_setup
 
-local which_key_is_available = simple_plugin_setup('which-key', 'which_key')
+local which_key_is_available = simple_plugin_setup('which-key', 'Ui.which_key')
 if not which_key_is_available then
   return
 end
