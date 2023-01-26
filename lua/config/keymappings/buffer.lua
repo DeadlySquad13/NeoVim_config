1743ce0011ebf268d7d6dab9720b2791021c6cae
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-01-26 21:14:16 +0300

    [Fix](Keymappings__Buffer): Added cmd to keymappings.

--- lua/config/keymappings/buffer.lua
+++ lua/config/keymappings/buffer.lua
@@ -2,11 +2,11 @@ return {
   name = 'Buffer',
 
   -- Deletes buffer without closing vim if it was in the only window.
-  d = { ':Bdelete<cr>', 'Delete' },
-  D = { '%bd|e#', 'Delete all buffer except for current one' },
+  d = { '<Cmd>Bdelete<Cr>', 'Delete' },
+  D = { '<Cmd>%bdelete|edit#<Cr>', 'Delete all buffer except for current one' },
 
   ---@see{the lua api at @link{https://github.com/akinsho/bufferline.nvim/blob/main/lua/bufferline.lua}}
   --p = { function() bufferline.pick_buffer() end, 'Pick' },
-  p = { '<cmd>BufferLinePick<cr>', 'Pick' },
-  n = { 'bn', 'Next' },
+  p = { '<Cmd>BufferLinePick<Cr>', 'Pick' },
+  n = { '<Cmd>bnext<Cr>', 'Next' },
 }
