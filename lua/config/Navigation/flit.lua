c2f4b47dffb340befc07dfa589021500d2babe5a
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-01-29 02:06:01 +0300

    [Enh](Flit): Disabled multiline as it crosses with leap.

--- lua/config/Navigation/flit.lua
+++ lua/config/Navigation/flit.lua
@@ -1,4 +1,9 @@
+local leap_settings = require('config.Navigation.leap.settings')
+
 return {
   labeled_modes = 'vo',
-  multiline = true,
+  multiline = false,
+  opts = {
+    safe_labels = leap_settings.safe_labels,
+  }
 }
