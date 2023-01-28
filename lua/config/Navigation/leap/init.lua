01c342ee36ece5085b9bfc7faca14f18fda38ee7
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-01-29 01:50:51 +0300

    [Enh](Leap): Changed keymappings in operator in visual modes to z/Z and x/X.

--- lua/config/Navigation/leap/init.lua
+++ lua/config/Navigation/leap/init.lua
@@ -1,5 +1,6 @@
 return {
   settings = require('config.Navigation.leap.settings'),
+  keymappings = require('config.Navigation.leap.keymappings'),
 
   commands = {
     multi_normal = require('config.Navigation.leap.multi_normal'),
