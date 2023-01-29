ae9c4ff1ff8a3842c749af19f3f360aa14232e8e
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-01-29 13:35:44 +0300

    [Enh](Sj): Added keymappings.

---
+++ lua/config/Navigation/sj/keymappings.lua
@@ -0,0 +1,24 @@
+local prequire = require('utils').prequire
+
+local sj_is_available, sj = prequire('sj')
+
+if not sj_is_available then
+  return 
+end
+
+-- Keymappings used during the search are in settings.
+return {
+  n = {
+    ['/'] = { sj.run, 'Search and jump' },
+    ['?'] = { function() sj.run({ forward_search = false }) end, 'Search and jump backwards' },
+    ['<Leader>'] = {
+      -- Default bindings that we have overriden in case you just want to iterate over search results.
+      ['/'] = { '/', 'Search' },
+      ['?'] = { '?', 'Search backwards' },
+
+      -- Like a leap but a little bit different.
+      s = { function() sj.run({ search_scope = 'visible_lines' }) end, 'Search and jump across visible lines' },
+      S = { function() sj.run({ search_scope = 'visible_lines', forward_search = false }) end, 'Search and jump across visible lines backwards' },
+    },
+  },
+}
--- lua/config/Navigation/sj/settings.lua
+++ lua/config/Navigation/sj/settings.lua
@@ -5,6 +5,9 @@ return {
   use_overlay = false, -- If true, apply an overlay to better identify labels and matches.
   labels = leap_settings.labels,
   relative_labels = true, -- if true, labels are ordered from the cursor position, 
+  update_search_register = true, -- If true, update the search register with the last used pattern.
+  -- Can be: current_line, visible_lines_above, visible_lines_below, visible_lines, buffer.
+  search_scope = 'buffer', -- To align more with default / and ?
 
   keymaps = {
     prev_pattern = "<C-k>", -- Select the previous pattern while searching.
