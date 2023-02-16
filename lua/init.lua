0aac0fcc9a8d6c56a0679be9677d8b1abc98025c
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-02-17 00:00:15 +0300

    [Fix](Keymappings): Moved create new line above and below to which-key to prevent plugin overrides.

--- lua/init.lua
+++ lua/init.lua
@@ -13,8 +13,8 @@ require('config.commands')
 require('autocommands')
 local colorschemas = require('config.theme')
 require('ds_omega.utils').load_coloscheme(
-  colorschemas.COLORSCHEME_NAME,
-  colorschemas.BACKUP_COLORSCHEME_NAME,
-  colorschemas.FALLBACK_COLORSCHEME_NAME
+    colorschemas.COLORSCHEME_NAME,
+    colorschemas.BACKUP_COLORSCHEME_NAME,
+    colorschemas.FALLBACK_COLORSCHEME_NAME
 )
 require('plugins')
