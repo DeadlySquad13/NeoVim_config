b609afa4338c22ab4a44b4a8ff75cad6a575940d
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-02-16 10:27:19 +0300

    [Enh](Keymappings): Added cutlass keymappings to x, surround - s, leap - m.

--- lua/config/Editing/surround.lua
+++ lua/config/Editing/surround.lua
@@ -1,1 +1,10 @@
-return {}
+return {
+    keymaps = {
+        -- See `:h nvim-surround.config.keymaps`.
+        normal = 's',
+        normal_cur = 'ss',
+        normal_line = 'S',
+        visual = 's',
+        visual_line = 'S',
+    },
+}
