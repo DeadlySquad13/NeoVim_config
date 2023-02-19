eba49b41223c42ec6a8870402e1bd50570dfc52a
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-02-19 12:50:45 +0300

    [Enh](VisualMulti): Renamed module and added some keymappings.

---
+++ lua/config/Editing/visual_multi/keymappings.lua
@@ -0,0 +1,6 @@
+-- Plugin has builtin keymapping management. See `:h g:VM_maps`.
+return {
+    ['Find Under'] = '<M-c>', -- C for cursor.
+    ['Add Cursor Up'] = '<M-k>',
+    ['Add Cursor Down'] = '<M-j>',
+}
