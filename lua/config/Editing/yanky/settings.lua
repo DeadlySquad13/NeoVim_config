b9db6ba5f3c87b675ba0bab68d0989b81c969034
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-02-19 12:43:55 +0300

    [Enh](Yanky): Added pickers.

--- lua/config/Editing/yanky/settings.lua
+++ lua/config/Editing/yanky/settings.lua
@@ -3,15 +3,21 @@ return {
     -- I don't like when system clipboard is intermingled with vim.
     sync_with_ring = false,
   },
+
   -- I have it already configured separately via autocmd.
   -- TODO: Adopt it here instead?
   highlight = {
     on_put = false,
     on_yank = false,
   },
+
   -- I have yank assassin for that.
   -- TODO: Make assassin optional.
   preserve_cursor_position = {
     enabled = false,
+  },
+
+  picker = {
+    telescope = require('config.Editing.yanky.picker').telescope,
   },
 }
