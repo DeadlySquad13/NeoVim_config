ae608ef2da99986e39053e45343695ebd997586d
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-02-19 12:45:09 +0300

    [Enh](Yanky): Loaded extension.

--- lua/ds_omega/layers/Editing/yanky.lua
+++ lua/ds_omega/layers/Editing/yanky.lua
@@ -1,3 +1,11 @@
 local simple_plugin_setup = require('ds_omega.utils').simple_plugin_setup
 
 simple_plugin_setup('yanky', 'Editing')
+
+local prequire = require('utils').prequire
+
+local telescope_is_available, telescope = prequire('telescope')
+
+if telescope_is_available then
+  telescope.load_extension('yank_history')
+end
