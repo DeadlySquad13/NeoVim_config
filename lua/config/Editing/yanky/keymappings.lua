55595e3bf2c2c4e6ced9c14638f60bbc16e587a1
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-02-27 00:47:16 +0300

    [Enh](Treesitter): Added gitcommits parser.

--- lua/config/Editing/yanky/keymappings.lua
+++ lua/config/Editing/yanky/keymappings.lua
@@ -58,8 +58,8 @@ local function get_put_keymappings(mode)
   return {
       p = { '<Plug>(YankyPutAfter)', 'Put after' },
       P = { '<Plug>(YankyPutBefore)', 'Put before' },
-      [';p'] = { '<Plug>(YankyGPutAfter)', 'G put after' },
-      [';P'] = { '<Plug>(YankyGPutBefore)', 'G put before' },
+      ['[p'] = { '<Plug>(YankyGPutAfter)', 'G put after' },
+      ['[P'] = { '<Plug>(YankyGPutBefore)', 'G put before' },
       -- Adjust indent to the current line.
       -- - Stay on pasted line.
       ['z'] = {
