dcffb94edcd0a3a8850ca1077a922c6d8c16957e
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-03-11 00:05:40 +0300

    [Enh](NeoVimDevelopment): Added yop.

--- lua/ds_omega/layers/NeoVimDevelopment/init.lua
+++ lua/ds_omega/layers/NeoVimDevelopment/init.lua
@@ -5,11 +5,17 @@ NeoVimDevelopment.plugins = {
   ['debuglog'] = {
     'smartpde/debuglog',
   },
+  ['yop'] = {
+    'zdcthomas/yop.nvim',
+  },
 }
 
 NeoVimDevelopment.configs = {
   ['debuglog'] = function()
     require('ds_omega.layers.NeoVimDevelopment.debuglog')
+  end,
+  ['yop'] = function()
+    require('ds_omega.layers.NeoVimDevelopment.yop')
   end,
 }
 
