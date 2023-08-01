8b9d5250bcbe325605442e720e06b0e53d81be45
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-08-01 13:48:47 +0300

    [Enh](Git): Added fugitive, neogit and diffview.

--- lua/ds_omega/config/Git/diffview/init.lua
+++ lua/ds_omega/config/Git/diffview/init.lua
@@ -1,3 +1,9 @@
 return {
-  settings = require('ds_omega.config.Git.diffview.settings'),
+  'sindrets/diffview.nvim',
+
+  dependencies = 'nvim-lua/plenary.nvim',
+
+  opts = require('ds_omega.config.Git.diffview.settings'),
+
+  config = true,
 }
