d98e6038af90e492830dac56e5fc1fee5c7f9fb4
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-08-16 12:42:58 +0300

    [Fix](Golang): Updated import paths.

--- after/ftplugin/go/init.lua
+++ after/ftplugin/go/init.lua
@@ -1,4 +1,4 @@
-local apply_bufferlocal_keymappings = require('config.Ui.which_key.utils').apply_bufferlocal_keymappings
+local apply_bufferlocal_keymappings = require('ds_omega.config.Ui.which_key.utils').apply_bufferlocal_keymappings
 
 apply_bufferlocal_keymappings('n', {
   ['<Cr>'] = { function() require('utils.exec').for_current_file({ 'go', 'run', '.' }) end, 'Run current project' },
