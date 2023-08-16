f424db89ef4ca28aa09a9aea7592aeb9c99a0a13
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-08-16 12:42:27 +0300

    [Fix](Lsp): Updated mason options.

--- lua/ds_omega/config/Lsp/mason.lua
+++ lua/ds_omega/config/Lsp/mason.lua
@@ -1,12 +1,14 @@
 return {
-    'williamboman/mason.nvim',
+  'williamboman/mason.nvim',
 
-    opts ={  
-      providers = {
-        'mason.providers.client',
-        'mason.providers.registry-api' -- This is the default provider. You can still include it here if you want, as a fallback to the client provider.
-      }
-    },
+  opts = {
+    PATH = 'append',
 
-    config = true,
+    providers = {
+      'mason.providers.client',
+      'mason.providers.registry-api'   -- This is the default provider. You can still include it here if you want, as a fallback to the client provider.
+    }
+  },
+
+  config = true,
 }
