271d066ed02aab2944dd65fa02735fd0b5f45b99
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-08-17 18:22:44 +0300

    [Enh](Navigation): Added live-grep from mini-files curdir and fixed utils item.

--- lua/ds_omega/config/Navigation/mini_files.lua
+++ lua/ds_omega/config/Navigation/mini_files.lua
@@ -26,18 +26,46 @@ return {
     local utils = require('ds_omega.utils')
     local create_augroup, create_autocmd = utils.create_augroup, utils.create_autocmd
 
-    local files_set_cwd = function(path)
+    local function mini_files_get_current_dir()
       -- Works only if cursor is on the valid file system entry
       local cur_entry_path = MiniFiles.get_fs_entry().path
       local cur_directory = vim.fs.dirname(cur_entry_path)
-      vim.fn.chdir(cur_directory)
+
+      return cur_directory
+    end
+
+    local mini_files_set_cwd = function()
+      vim.fn.chdir(mini_files_get_current_dir())
     end
 
     create_autocmd('User', {
       pattern = 'MiniFilesBufferCreate',
       desc = 'Add change current directory to current directory inside MiniFiles',
       callback = function(args)
-        vim.keymap.set('n', 'cd', files_set_cwd, { buffer = args.data.buf_id })
+        vim.keymap.set('n', 'cd', mini_files_set_cwd, { buffer = args.data.buf_id })
+      end,
+    })
+
+    local function mini_files_live_grep()
+      local prequire = require('ds_omega.utils').prequire
+
+      local builtin_is_available, builtin = prequire('telescope.builtin')
+
+      if not builtin_is_available or not builtin then
+        return
+      end
+
+      builtin.live_grep({
+        prompt_title = 'Live Grep from MiniFiles',
+        cwd = mini_files_get_current_dir(),
+      })
+    end
+
+    create_autocmd('User', {
+      pattern = 'MiniFilesBufferCreate',
+      desc = 'Live grep from current directory inside MiniFiles',
+      callback = function(args)
+        vim.keymap.set('n', '<Leader>ng', mini_files_live_grep, { buffer = args.data.buf_id })
       end,
     })
   end,
