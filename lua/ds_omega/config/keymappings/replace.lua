7898ccf38ba95609728a4e1976e9db2923f17d42
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-04-22 19:25:46 +0300

    [Chore]: Removed search-replace.nvim because substitute.nvim is enough (and better).

--- lua/ds_omega/config/keymappings/replace.lua
+++ lua/ds_omega/config/keymappings/replace.lua
@@ -1,23 +1,10 @@
-local cmd = require('hydra.keymap-util').cmd
-
----@param command (string)
-local function search_replace(command)
-    return cmd('SearchReplace' .. command)
-end
-
 -- # Replace. Search and replace mappings.
 -- Use    ['<Right>'] = { 'l', 'Right' }, as motion over range for substitute nvim.
 local replace_mappings = {
     name = 'Replace',
     -- vnoremap <leader>sw <esc>:%s;\<<c-r><c-w>\>;;g<left><left>
-    -- Don't really remember why I needed it...
+    -- For replacing tags.
     t = { [[:%s;<\w*>\(<\\\w*>\)\?;;g<left><left>]], 'Tag' },
-
-
-    o = { search_replace 'SingleBufferOpen', "Open" },
-    a = { search_replace 'SingleBufferCWord', "Word" },
-    A = { search_replace 'SingleBufferCWORD', "CWord" },
-    e = { search_replace 'SingleBufferCExpr', "C expression" },
 }
 
 return replace_mappings
