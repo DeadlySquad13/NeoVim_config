0fd7bc46491624429a3e9b8595f0c65f1177242e
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-07-01 15:14:45 +0000

    [Refactor](Keymappings__QuickfixList): Used cmd and pcmd utils from our module.

--- lua/ds_omega/config/keymappings/quickfix_list.lua
+++ lua/ds_omega/config/keymappings/quickfix_list.lua
@@ -1,7 +1,15 @@
 local Hydra = require('hydra')
 
-local cmd = require('hydra.keymap-util').cmd
-local pcmd = require('hydra.keymap-util').pcmd
+local prequire = require('ds_omega.utils').prequire
+
+local utils_is_available, utils = prequire('ds_omega.config.keymappings._common.utils')
+
+if not utils_is_available then
+  return
+end
+
+local cmd = utils.cmd
+
 local CONSTANTS = require("ds_omega.config.keymappings._common.constants")
 local keymappings, transitive_catalizator = CONSTANTS.keymappings, CONSTANTS.transitive_catalizator
 
