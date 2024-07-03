c2323fcd1407a25fc8d60fe96a6198690d49ac24
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-07-03 18:46:09 +0000

    [Refactor](Recorder): Made macro key a variable.
    Have caught bug with lq instead lj for deletion so decided to move it into variable.

--- lua/ds_omega/config/Editing/recorder/settings.lua
+++ lua/ds_omega/config/Editing/recorder/settings.lua
@@ -1,11 +1,13 @@
+local MACRO_KEYS = { "j", "J", "<C-j>" }
+
 return {
     mapping = {
-        startStopRecording = "j",
-        playMacro = "J",
-        switchSlot = "<C-j>",
-        editMacro = "mj",
-        yankMacro = "fj",
-        deleteAllMacros = "lq",
+        startStopRecording = MACRO_KEYS[1],
+        playMacro = MACRO_KEYS[2],
+        switchSlot = MACRO_KEYS[3],
+        editMacro = "m"..MACRO_KEYS[1],
+        yankMacro = "f"..MACRO_KEYS[1],
+        deleteAllMacros = "l"..MACRO_KEYS[1],
         addBreakPoint = '<Leader>db', -- 'Debug add Break point'.
     },
 }
