1785267fa20353125c3cadf398b46ca5fe938819
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-07-18 10:50:20 +0000

    [Fix](Eiting): Added select last put region.
    Finally found a way to `gp`.

--- lua/ds_omega/config/Editing/yanky/settings.lua
+++ lua/ds_omega/config/Editing/yanky/settings.lua
@@ -14,8 +14,14 @@ return function()
       on_yank = false,
     },
 
-    -- I have yank assassin for that.
-    -- TODO: Make assassin optional.
+    -- Text object corresponding to last put text. Especially useful with Lsp
+    -- because builtin NeoVim put marks are sometimes overriden.
+    textobj = {
+      enabled = true,
+    },
+
+    -- I use YankAssassin for that because it's easier to integrate with other
+    -- mappings.
     preserve_cursor_position = {
       enabled = false,
     },
