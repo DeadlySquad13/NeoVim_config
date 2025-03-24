a2f325494ce2b9a17b2345164a18057969368d35
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-03-24 20:42:24 +0300

    [Chore]: Removed unused layer files.

--- lua/ds_omega/layers/Navigation/quick_scope.lua
+++
@@ -1,5 +0,0 @@
-local setters = require('ds_omega.utils.setters')
-
-local config = require('ds_omega.config.Navigation.quick_scope')
-
-setters.set_global_variables(config)
--- lua/ds_omega/layers/TextObjects/smart_word.lua
+++
@@ -1,4 +0,0 @@
-local prequire = require('ds_omega.utils').prequire
-
--- Just activate hydra.
-prequire('ds_omega.config.TextObjects.word')
