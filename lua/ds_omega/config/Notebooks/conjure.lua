a8c58147e137fba3421a6e8c731b1ac8868ef415
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-03-30 11:29:36 +0000

    [Style](Conjure)

--- lua/ds_omega/config/Notebooks/conjure.lua
+++ lua/ds_omega/config/Notebooks/conjure.lua
@@ -7,7 +7,6 @@ return {
     -- Optional cmp-conjure integration
     dependencies = { "PaterJason/cmp-conjure" },
 
-    'Olical/conjure',
     config = function()
       vim.g['conjure#mapping#doc_word'] = '<Leader>ii'
     end,
@@ -19,7 +18,8 @@ return {
       local cmp = require("cmp")
       local config = cmp.get_config()
       table.insert(config.sources, { name = "conjure" })
-      return cmp.setup(config)
+
+      cmp.setup(config)
     end,
   },
 }
