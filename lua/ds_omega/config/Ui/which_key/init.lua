ef6585c6a335f86d8ef680a1983ee9976f646e3a
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-06-08 15:05:48 +0300

    [Fix](Langmapper): Made proper config, removed unnecessary hacks.

--- lua/ds_omega/config/Ui/which_key/init.lua
+++ lua/ds_omega/config/Ui/which_key/init.lua
@@ -14,20 +14,6 @@ return {
 
     opts = require('ds_omega.config.Ui.which_key.settings'),
     config = function(_, opts)
-        local lmu = require('langmapper.utils')
-        local view = require('which-key.view')
-        local execute = view.execute
-
-        -- wrap `execute()` and translate sequence back
-        view.execute = function(prefix_i, mode, buf)
-            -- Translate back to English characters
-            local ru_prefix_i = lmu.translate_keycode(prefix_i, 'default', 'ru')
-            execute(ru_prefix_i, mode, buf)
-
-            -- local hdn_prefix_i = lmu.translate_keycode(prefix_i, 'default', 'hdn')
-            -- execute(hdn_prefix_i, mode, buf)
-        end
-
         require('which-key').setup(opts)
 
         local apply_keymappings = require('ds_omega.config.Ui.which_key.utils').apply_keymappings
