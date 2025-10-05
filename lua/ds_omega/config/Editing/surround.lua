c8bf5966917ff59828edd0300c16daa947aae6ef
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-10-05 12:40:35 +0300

    [Enh](Surround): Added markdown block surround.

--- lua/ds_omega/config/Editing/surround.lua
+++ lua/ds_omega/config/Editing/surround.lua
@@ -18,15 +18,34 @@ return {
       change_line = 'mT',
     },
 
+    surrounds = {
+      -- Surround with markdown code block, triple backticks.
+      -- <https://github.com/kylechui/nvim-surround/issues/88>
+      ["c"] = {
+        add = function()
+          local ft = vim.bo.filetype
+          if ft ~= "markdown" then
+            return
+          end
+
+          local config = require("nvim-surround.config")
+          local result = config.get_input("Code block language: ")
+
+          return {
+            { "```" .. result, '' },
+            { "",                "```" },
+          }
+        end,
+      },
+    },
+
     aliases = {
-        q = { '"', "'", "`" },
-        t = { "}", "]", ")", ">", '"', "'", "`" },
-        -- We have text-objects for function mapped to `m` as in "method".
-        m = "f",
+      q = { '"', "'", "`" },
+      t = { "}", "]", ")", ">", '"', "'", "`" },
+      -- We have text-objects for function mapped to `m` as in "method".
+      m = "f",
     }
   },
 
   config = true,
 }
-
-
