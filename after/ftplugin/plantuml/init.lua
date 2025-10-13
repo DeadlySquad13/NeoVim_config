efb5494fe4081879c19c97cf7d81961e5b7be566
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-10-13 17:43:09 +0300

    [Fix](Plantuml): Added alternative compilation way to 'run' keymapping.

--- after/ftplugin/plantuml/init.lua
+++ after/ftplugin/plantuml/init.lua
@@ -1,5 +1,17 @@
 local apply_bufferlocal_keymappings = require('ds_omega.config.Ui.which_key.utils').apply_bufferlocal_keymappings
 
+local plantuml_cmd = [[<Cmd>:!plantuml % -tpng && plantuml % -tsvg<Cr>]]
+local kroki_cmd = [[<Cmd>:!kroki convert % --type plantuml && kroki convert % --type plantuml --format png<Cr>]]
+
 apply_bufferlocal_keymappings('n', {
-  ['<Cr>'] = { [[<Cmd>:!kroki convert % --type plantuml && kroki convert % --type plantuml --format png<Cr>]], 'Convert current file to svg and png' },
+  ['<Cr>'] = {
+    -- Unfortunately, includes didn't work when working with kroki in
+    -- docker container. Kroki is still ok for most cases so leaving as
+    -- fallback.
+    --
+    -- May be related:
+    -- https://github.com/yuzutech/kroki/issues/1638
+    vim.fn.executable('plantum') and plantuml_cmd or kroki_cmd,
+    'Convert current file to svg and png',
+  },
 })
