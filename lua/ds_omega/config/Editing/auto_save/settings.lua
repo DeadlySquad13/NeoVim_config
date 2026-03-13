972923be6924bfc3ed957bd85da3ee6d9f74be87
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2026-03-13 13:46:11 +0300

    [Chore](AutoSave): Disabled on some aux filetypes.

--- lua/ds_omega/config/Editing/auto_save/settings.lua
+++ lua/ds_omega/config/Editing/auto_save/settings.lua
@@ -1,3 +1,24 @@
+-- Some recommended exclusions. you can use `:lua print(vim.bo.filetype)` to
+-- get the filetype string of the current buffer.
+local excluded_filetypes = {
+    -- this one is especially useful if you use neovim as a commit message editor
+    -- "gitcommit",
+    -- Quickfixlist changer.
+    "replacer",
+    -- most of these are usually set to non-modifiable, which prevents autosaving
+    -- by default, but it doesn't hurt to be extra safe.
+    "NvimTree",
+    "Outline",
+    "TelescopePrompt",
+    "alpha",
+    "dashboard",
+    "lazygit",
+    "neo-tree",
+    "oil",
+    "prompt",
+    "toggleterm",
+}
+
 return {
     trigger_events = {                                 -- See :h events
         immediate_save = { "BufLeave", "FocusLost" },  -- vim events that trigger an immediate save
@@ -9,10 +30,7 @@ return {
 
         -- Don't save for these file types.
         return not vim.list_contains(
-            {
-                -- Quickfixlist changer.
-                "replacer",
-            },
+            excluded_filetypes,
             filetype
         )
     end,
