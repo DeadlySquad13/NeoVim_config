9071e269b723e6ed82fdddf5493430a72522f33b
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2026-05-12 14:51:13 +0300

    [Chore](Aerojump): Changed structure for our recent plugin flow.

--- lua/ds_omega/config/Navigation/aerojump/init.lua
+++ lua/ds_omega/config/Navigation/aerojump/init.lua
@@ -1,24 +1,31 @@
+local M = {}
+
+M.keymappings = require('ds_omega.config.Navigation.aerojump.keymappings')
+-- `apply_plugin_keymappings` will not accept non-mode keys in a keymappings
+-- table.
+M.search_keymappings = M.keymappings.search
+M.keymappings.search = nil
+
 return {
   'ripxorip/aerojump.nvim',
 
   build = ':UpdateRemotePlugins',
 
+  keys = to_lazy_keys(M.keymappings),
+
   config = function(_, opts)
-    local ds_omega_utils_is_available, ds_omega_utils = prequire('ds_omega.ds_omega_utils')
+    local ds_omega_utils_is_available = prequire('ds_omega.ds_omega_utils')
+
     if not ds_omega_utils_is_available then
       return
     end
 
-    local keymappings = require('ds_omega.config.Navigation.aerojump.keymappings')
-    -- `apply_plugin_keymappings` will not accept non-mode keys in a keymappings
-    -- table.
-    local search_keymappings = keymappings.search
-    keymappings.search = nil
-
-    ds_omega_utils.apply_plugin_keymappings(keymappings)
+    local ds_omega_utils = require('ds_omega.ds_omega_utils')
+    
+    ds_omega_utils.apply_plugin_keymappings(M.keymappings)
 
     local setters = require('ds_omega.utils.setters')
 
-    setters.set_global_variables({ keymaps = search_keymappings }, 'aerojump')
+    setters.set_global_variables({ keymaps = M.search_keymappings }, 'aerojump')
   end,
 }
