cdf485dc091d2899cebeec4cbc0b43625a2b7ac2
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2026-07-08 18:41:41 +0300

    [Fix](OpencodeLauncher__Docker): Adapted docker command for rootless installation.

--- lua/ds_omega/modules/opencode_launcher/strategies/config.lua
+++ lua/ds_omega/modules/opencode_launcher/strategies/config.lua
@@ -55,18 +55,37 @@ M.DockerStrategy = M.register_strategy({
         os.execute(string.format("docker stop %s 2>/dev/null || true", container_name))
 
         logging.log(string.format("Starting container: %s on port %d", container_name, port))
+        -- Reference: https://github.com/pilinux/opencode-docker/blob/main/Dockerfile
+        -- - 'node' is a user with UID matching to current user of the system.
+        -- It can leverage volumes properly. There's also 'opencode' user - but
+        -- it's hard to use volumes with it - it's mostly for one-shot work
+        -- that doesn't need persistent storage (so we usually use 'node's
+        -- files).
+        -- - `/app` is for agent work. Everything is writable.
+        -- - `/home/node` is for 'node' user dotfiles. Everything is writable for node user.
+        --   - we mount our configs to it's home. Because UIDs match
+        --   - everything works nicely. The only thing we take care of is
+        --   making `.config/opencode` readonly so that agent doesn't change
+        --   it's own configuration and permissions.
+        -- - `/home/opencode` is for 'opencode' user dotfiles. Everything is writable for opencode user.
+        -- FIX: Note that state and history is still shared between agents!
+        -- WARN: We use `--user root` because we have rootless docker and all
+        -- volumes are mounted with 0:0 permissions. On a system that runs
+        -- Docker via root, it may pose security problems.
         local cmd = string.format([[
 docker run -d --rm \
 --name %s \
 -p %d:4096 \
--v ~/.local/state/opencode:/home/node/.local/state/opencode \
--v ~/.local/share/opencode:/home/node/.local/share/opencode \
--v ~/.config/opencode:/home/node/.config/opencode \
--v "%s":/app:rw \
+-v ~/.local/state/opencode:/root/.local/state/opencode \
+-v ~/.local/share/opencode:/root/.local/share/opencode \
+-v ~/.config/opencode:/root/.config/opencode:ro \
+-v "%s":/app \
+--user root \
 ghcr.io/pilinux/opencode:latest opencode serve --port 4096 --hostname '0.0.0.0']],
             container_name,
             port,
-            cwd
+            cwd,
+            container_name
         )
 
         logging.notify("Starting opencode")
