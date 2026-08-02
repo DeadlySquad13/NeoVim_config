862f89440c1956af8f286528c92cc5eb9e3bad1c
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2026-08-02 18:46:56 +0300

    [Enh](OpencodeLauncher__Strategies): Added proper check for existing container.
    Also refactored Docker strategy to reuse this code.

--- lua/ds_omega/modules/opencode_launcher/strategies/config.lua
+++ lua/ds_omega/modules/opencode_launcher/strategies/config.lua
@@ -5,6 +5,29 @@ M.logger = get_logger('OpencodeLanuncher__Strategies')
 M.strategies = {}
 
 ---
+---@param container_name (string)
+---@param opts? ({ sudo: boolean }|nil)
+---@return
+M.check_container_exists = function(container_name, opts)
+    local check_cmd = string.format('docker ps --filter "name=^%s$" --format "{{.Names}}"', container_name)
+    if opts and opts.sudo then
+        check_cmd = string.format(
+            'SUDO_ASKPASS="%s/sudo-askpass.sh" sudo -A %s',
+            require('ds_omega.constants.env').SHARED_SCRIPTS,
+            check_cmd
+        )
+    end
+
+    local handle = io.popen(check_cmd)
+    local result = handle:read("*a")
+    handle:close()
+
+    local container_exists = result and string.match(result, vim.pesc(container_name))
+
+    return container_exists, result
+end
+
+---
 ---@param config (OpencodeOpenStrategyConfig)
 ---@return (OpencodeOpenStrategy) strategy
 M.register_strategy = function(config)
@@ -41,12 +64,9 @@ M.DockerStrategy = M.register_strategy({
         local container_name = string.format('opencode-%s', dir_name)
 
         logging.log(string.format("Checking if container '%s' already exists", container_name))
-        local check_cmd = string.format('docker ps --filter "name=%s" --format "{{.Names}}"', container_name)
-        local handle = io.popen(check_cmd)
-        local result = handle:read("*a")
-        handle:close()
+        local container_exists, _ = M.check_container_exists(container_name)
 
-        if result and result:match(container_name) then
+        if container_exists then
             logging.notify(string.format("Container %s is already running, skipping start", container_name))
             return true
         end
@@ -117,27 +137,26 @@ M.DarkGreenTangerineDreamStrategy = M.register_strategy({
         local notifier = M.logger:get_notifier({ title = self.name })
 
         notifier:info(string.format("Checking if container '%s' already exists", container_name))
-        -- TODO: Workaround sudo via invoke and passkey.
-        -- local check_cmd = string.format('docker ps --filter "name=%s" --format "{{.Names}}"', container_name)
-        -- local handle = io.popen(check_cmd)
-        -- local result = handle:read("*a")
-        -- handle:close()
+        local container_exists, _ = M.check_container_exists(container_name, { sudo = true })
 
-        -- if not (result and result:match(container_name)) then
-        --     return false
-        -- end
+        if not container_exists then
+            notifier:warning(string.format("Container '%s' doesn't exist", container_name))
+            return false
+        end
 
         notifier:info(string.format("Container %s is running, progressing...", container_name))
 
         logging.notify(string.format("Starting OpenCode in container: %s on port %d", container_name, port))
 
-        return os.execute(string.format("invoke llm.serve-conv-plat-prov-opencode --system-name='darkGreen-tangerineDream' --port %d >/dev/null &", port))
+        return os.execute(string.format(
+            "invoke llm.serve-conv-plat-prov-opencode --system-name='darkGreen-tangerineDream' --port %d >/dev/null &",
+            port))
     end,
     kill_command = function(self, port)
         local container_name = "AiAssistance__darkGreen-tangerineDream"
 
         logging.notify(string.format("Stopping OpenCode server on port %d in container: %s", port, container_name))
-        return os.execute(string.format("invoke llm.kill-conv-plat-prov-opencode --port '%d' 2>/dev/null", port))
+        return os.execute(string.format("invoke llm.kill-conv-plat-prov-opencode --port '%d' > /dev/null", port))
     end
 })
 
