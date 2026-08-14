05b19886fa53c5739b1bdc78d295fa774949d8dd
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2026-08-14 13:40:06 +0300

    [Enh](ProjcetManagement__AutoSession): Enabled session flow even when params are passed to nvim cli.

--- lua/ds_omega/config/ProjectManagement/auto_session.lua
+++ lua/ds_omega/config/ProjectManagement/auto_session.lua
@@ -13,6 +13,11 @@ return {
     auto_restore_enabled = nil,
     auto_session_suppress_dirs = nil,
     auto_session_use_git_branch = true,
-    bypass_session_save_file_types = nil
+    bypass_session_save_file_types = nil,
+
+
+    -- Argument handling
+    args_allow_single_directory = true, -- Follow normal session save/load logic if launched with a single directory as the only argument
+    args_allow_files_auto_save = true, -- Allow saving a session even when launched with a file argument (or multiple files/dirs). It does not load any existing session first. Can be true or a function that returns true when saving is allowed. See documentation for more detail
   },
 }
